79ff56dd52a794057fbb3a752ce6cea2c564db2a
Rani Jaiswal <ruj96@live.com>
2017-03-29 00:15:25 -0400

    deleted database thing from settings

--- PrincetonPool/settings.py
+++ PrincetonPool/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'PrincetonPool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
