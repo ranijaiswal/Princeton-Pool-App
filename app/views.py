c09412b43ab9440c0c5577bd6b00948fa26b50ea
liucindy <cindyliu@princeton.edu>
2017-04-08 16:36:29 -0400

    configure landing page

--- app/views.py
+++ app/views.py
@@ -5,10 +5,34 @@ from django.contrib.auth.decorators import login_required
 import os
 # Create your views here.
 
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def index(request):
 	context = {
 		'Title': 'Welcome to Princeton Pool!',
 	}
-	
-	return render(request, 'app/main.html', context)
+
+	return render(request, 'app/index.html', context)
+
+def about(request):
+	context = {
+		'Title': 'About Us',
+	}
+	return render(request, 'app/about.html', context)
+
+def faq(request):
+	context = {
+		'Title': 'FAQs',
+	}
+	return render(request, 'app/faq.html', context)
+
+def open_airport(request):
+	context = {
+		'Title': 'List of open airport requests',
+	}
+	return render(request, 'app/open_requests.html', context)
+
+def open_shopping(request):
+	context = {
+		'Title': 'List of open shopping requests',
+	}
+	return render(request, 'app/open_requests.html', context)
