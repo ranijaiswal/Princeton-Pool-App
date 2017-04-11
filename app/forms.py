010eec63be701e8e975795d290986a9687a54f09
Annie Chen <chen.a.edna@gmail.com>
2017-04-11 12:50:08 -0400

    modified form

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,6 @@ class RequestForm(forms.Form):
 	email = forms.EmailField(label="Email", max_length=100)
 	destination = forms.CharField(label="Destination", max_length=100)
 	number_going = forms.IntegerField(max_value=10, min_value=1)
-	date = forms.DateField()
-	time = forms.TimeField()
+	date = forms.DateField(label="MM/DD/YYYY")
+	time = forms.TimeField(label="HH:MM, military time")
 	
