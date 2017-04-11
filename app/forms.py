3a8b095c111c771afce59d8a13c50baeb40391b0
Annie Chen <chen.a.edna@gmail.com>
2017-04-11 12:57:17 -0400

    modified form labels

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,6 @@ class RequestForm(forms.Form):
 	email = forms.EmailField(label="Email", max_length=100)
 	destination = forms.CharField(label="Destination", max_length=100)
 	number_going = forms.IntegerField(max_value=10, min_value=1)
-	date = forms.DateField(label="MM/DD/YYYY")
-	time = forms.TimeField(label="HH:MM, military time")
+	date = forms.DateField(label="Date (MM/DD/YYYY)")
+	time = forms.TimeField(label="Time (HH:MM)")
 	
