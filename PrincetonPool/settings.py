a34e7cdffa96d59798b2f31b6f6d14a37d7ea3cf
Rani Jaiswal <ruj96@live.com>
2017-04-14 15:00:41 -0400

    attempt at heroku postgres

--- PrincetonPool/settings.py
+++ PrincetonPool/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import dj_database_url
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -101,13 +101,15 @@ WSGI_APPLICATION = 'PrincetonPool.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
 #     }
 # }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+DATABASES['default'] =  dj_database_url.config()
 '''
 
 DATABASES = {
