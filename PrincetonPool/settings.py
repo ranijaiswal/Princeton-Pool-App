b2f4104c6dac9a05146383312680c75472241c42
Rani Jaiswal <ruj96@live.com>
2017-04-30 08:40:47 -0400

    first

--- PrincetonPool/settings.py
+++ PrincetonPool/settings.py
@@ -94,6 +94,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': ['django.contrib.staticfiles.templatetags.staticfiles']
         },
     },
 ]
