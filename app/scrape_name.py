bf5e5668b3bbf5d349fff66606d45affceb8e438
Annie Chen <chen.a.edna@gmail.com>
2017-05-08 17:00:47 -0400

    modified scrape_name

--- app/scrape_name.py
+++ app/scrape_name.py
@@ -8,13 +8,28 @@ def scrape_name(netid):
 
 	r = requests.get(search_url + netid)
 	data=r.text
-	soup=BeautifulSoup(data, 'lxml')
+	soup=BeautifulSoup(data)
 
 	name = soup.find(id = 'people-row-link-3')
-	just_name = str(name).split("\n")[1]
-	# print just_name
-	clean_name = just_name.split(",")
-	# print clean_name
-	final_name = clean_name[1].strip() 
+	scraped_name = str(name).split("\n")[1]
+	just_name = scraped_name.split(",")
 
-	return final_name
+	last_name = just_name[0].strip() 
+	fname_array = just_name[1].split()
+
+	if "." in fname_array[-1]:
+		first_name=fname_array[0:-1] #new array without period name
+	else:
+		first_name=fname_array
+
+	fname_str = ""
+	for index, n in enumerate(first_name):
+		fname_str += n
+		if index != len(first_name)-1:
+			fname_str += " "
+
+	name_list = []
+	name_list.append(fname_str)
+	name_list.append(last_name)
+
+	return name_list
