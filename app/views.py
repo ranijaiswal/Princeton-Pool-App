710a1ec674d82d331ac1aa05a767a7ebd08792ca
liucindy <cindyliu@princeton.edu>
2017-05-10 00:40:22 -0400

    drop ride earlier

--- app/views.py
+++ app/views.py
@@ -366,6 +366,11 @@ def drop_ride(request, ride_id):
 	ride.save()
 	rider.save()
 
+	#make sure this is the last thing done in the view
+	if (ride.usrs.count() == 0):
+		ride.delete()
+	return render(request, 'app/drop_ride.html', context)
+
 	# list of all the riders
 	riders_emails = []
 	riders_firstnames = ""
@@ -435,11 +440,6 @@ def drop_ride(request, ride_id):
     "<p>This is a simple HTML email body</p>", "text/html" #don't know what this does but it doesn't work w/o it, don't delete
 	)
 	mail_to_riders.send()
-
-	#make sure this is the last thing done in the view
-	if (ride.usrs.count() == 0):
-		ride.delete()
-	return render(request, 'app/drop_ride.html', context)
 
 
 class RidesList(generics.ListAPIView):
